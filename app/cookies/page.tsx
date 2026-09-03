ffef4298e6a5ba479560490fa915f608d25131e2
MICHAEL KASINGYE <michaelkasingye@gmail.com>
2026-09-03 11:46:26 +0300

    Fix email address in cookies policy page

--- app/cookies/page.tsx
+++ app/cookies/page.tsx
@@ -72,7 +72,7 @@ export default function CookiesPage() {
                   current version of this policy is always available on this page.
                 </p>
                 <p>
-                  For questions about this policy, contact us at <a href="mailto:info@aclpit.org">info@aclpit.org</a>.
+                  For questions about this policy, contact us at <a href="mailto:info@aclpit.org">info@aclpit.com</a>.
                   See also our <Link href="/privacy">Privacy Policy</Link>.
                 </p>
               </div>
